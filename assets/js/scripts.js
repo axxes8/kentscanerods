886980075088dbce35fe42fa689ac6f8c3085cbe
BuildTools <unconfigured@null.spigotmc.org>
2025-01-31 02:50:37 -0700

    images are clickable. added js files for that.

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+    /***************** Initiate Fancybox ******************/
+
+    $('.single_image').fancybox({
+        padding: 4
+    });
+
+    $('.fancybox').fancybox({
+        padding: 4,
+        width: 1000,
+        height: 800
+    });
